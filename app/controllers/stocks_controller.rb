0d8ae2a7000989c7e65ea08b3dc6aed3549e1ebf
callumahern <callum.ahern@simplybusiness.co.uk>
2023-01-03 13:50:37 +0000

    rubocop

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,16 @@
-class StocksController < ApplicationController
+# frozen_string_literal: true
 
+class StocksController < ApplicationController
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
         render 'users/my_portfolio'
       else
-        flash[:alert] = "Please enter a valid symbol to search"
+        flash[:alert] = 'Please enter a valid symbol to search'
         redirect_to my_portfolio_path
       end
-    elsif 
-      flash[:alert] = "Please enter a symbol to search"
+    elsif flash[:alert] = 'Please enter a symbol to search'
       redirect_to my_portfolio_path
     end
   end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class Stock < ApplicationRecord
-
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-                                  
+
     begin
-      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
-    rescue => exception
-      return nil
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name,
+          last_price: client.price(ticker_symbol))
+    rescue StandardError => e
+      nil
     end
   end
 end
